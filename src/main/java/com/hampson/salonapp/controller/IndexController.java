950146ea3c41253134205235413258e00cf4328a
Aaron Hampson <aaron0819@users.noreply.github.com>
2017-03-12 21:43:44 -0400

    IndexController Update
    Updated index controller to check for active account and bypass login page if true

--- src/main/java/com/hampson/salonapp/controller/IndexController.java
+++ src/main/java/com/hampson/salonapp/controller/IndexController.java
@@ -5,13 +5,21 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
 	@RequestMapping("/")
-	public String index(HttpServletRequest request, HttpServletResponse response) {
-
-		return "index";
+	public ModelAndView index(HttpServletRequest request, HttpServletResponse response) {
+		
+		String page = "index";
+		
+		//Bypass login / registration page if active account already logged in 
+		if(null != request.getSession().getAttribute("pageIndicator")) {
+			page = "/login";
+		}
+		
+		return new ModelAndView(page);
 	}
 }
