52a49724744106db0235135e39fc50d867d683be
Aaron Hampson <aaron0819@users.noreply.github.com>
2017-04-12 19:34:58 -0400

    Add files via upload

--- src/main/java/com/hampson/salonapp/controller/VerificationController.java
+++ src/main/java/com/hampson/salonapp/controller/VerificationController.java
@@ -8,17 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hampson.salonapp.service.AccountService;
-import com.hampson.salonapp.service.AppointmentService;
 
 @Controller
 public class VerificationController {
 
-	private AppointmentService appointmentService;
-
 	@RequestMapping("/verify")
 	public ModelAndView verifyAccount(@RequestParam("verificationCode") String verificationCode,
 			@RequestParam("emailAddress") String emailAddress, HttpServletRequest request) {
-		ModelAndView mav = new ModelAndView("index");
+		ModelAndView mav = new ModelAndView("redirect:/login");
 		AccountService accountService = new AccountService();
 
 		String[] account = accountService.retrieveAccountByEmailAddress(emailAddress);
@@ -32,19 +29,11 @@ public class VerificationController {
 				if (Integer.parseInt(account[0]) > 0) {
 					request.getSession().setAttribute("pageIndicator", 1);
 					request.getSession().setAttribute("customerId", Integer.parseInt(account[0]));
-					mav = new ModelAndView("customerHome");
-					mav.addObject("appointments", appointmentService
-							.getAppointmentsByCustomerId((Integer) request.getSession().getAttribute("customerId")));
-					mav.addObject("pendingAppointments", appointmentService.getPendingAppointmentsByCustomerId((Integer) request.getSession().getAttribute("customerId")));
-
+					System.out.println("customer");
 				} else if (Integer.parseInt(account[1]) > 0) {
 					request.getSession().setAttribute("pageIndicator", 2);
 					request.getSession().setAttribute("stylistId", Integer.parseInt(account[1]));
-					mav = new ModelAndView("stylistHome");		
-					mav.addObject("appointments", appointmentService
-							.getAppointmentsByStylist((Integer) request.getSession().getAttribute("stylistId")));
-					mav.addObject("myPendingAppointments", appointmentService.getPendingAppointmentsByStylistId((Integer) request.getSession().getAttribute("stylistId")));
-					mav.addObject("noPreferencePendingAppointments", appointmentService.getPendingAppointmentsWithNoStylistPreference());
+					System.out.println("stylist");
 				}
 			}
 		}
